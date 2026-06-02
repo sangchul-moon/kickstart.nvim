5a96b4e108403153a88aa94c430da0f3bea00c4e
Sangchul Moon <gidongwii@gmail.com>
2026-06-02 09:23:34 +0900

    set color theme to solarized dark

--- init.lua
+++ init.lua
@@ -383,9 +383,9 @@ do
   -- change the command under that to load whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  vim.pack.add { gh 'folke/tokyonight.nvim' }
+  vim.pack.add { gh 'maxmx03/solarized.nvim' }
   ---@diagnostic disable-next-line: missing-fields
-  require('tokyonight').setup {
+  require('solarized').setup {
     styles = {
       comments = { italic = false }, -- Disable italics in comments
     },
@@ -394,7 +394,8 @@ do
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'solarized'
+  vim.o.background = 'dark'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
